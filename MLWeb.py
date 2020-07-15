5bce10d2756bebd8950f00d0d38208243d3f1273
Balaji Padamwar <balajipadamwarp@gmail.com>
2020-07-15 14:49:42 +0530

    IBMHackchallenge

---
+++ MLWeb.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 19:34:12 2020
+
+@author: KISHAN
+"""
+
+import pickle
+from flask import Flask, request, render_template, send_file
+from twitterscraper import query_tweets
+import datetime as dt
+import pandas as pd
+import random
+from gtts import gTTS 
+import os
+from playsound import playsound
+import matplotlib.pyplot as plt
+import numpy as np
+
+z=1
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+#CORS(app)
+
+vectorize = pickle.load(open('VECT.sav', 'rb'))
+classifier = pickle.load(open('SVM.sav', 'rb'))
+@app.route("/", methods=['GET','POST'])
+def return_sentiment():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':       
+        sentence = request.form['sentence']
+        if(sentence):
+            sentence_vector = vectorize.transform([sentence])
+            sentiment = classifier.predict(sentence_vector).tolist()
+            return render_template('index.html', original_input= {'sentence':sentence}, result = sentiment[0])
+
+@app.route("/scrape/",methods=['GET','POST'])
+def return_data():
+    if request.method == 'POST':
+        today = dt.date.today()
+        yesterday = today - dt.timedelta(days = 1)
+        begin_date=yesterday
+        end_date = today 
+        limit = 100
+        lang='en'
+        tweets = query_tweets("corona",begindate = begin_date,enddate = end_date ,limit=limit ,lang=lang)
+        df = pd.DataFrame(t.__dict__ for t in tweets)
+        i=df.text.str.strip()
+        j=df.timestamp
+        l=random.randint(1,len(i))
+        tweet=i[l]
+        tweet_vector = vectorize.transform([tweet])
+        sent = classifier.predict(tweet_vector).tolist()
+        mytext = tweet
+        language = 'en'
+        myobj = gTTS(text = mytext, lang = language, slow = False)
+        myobj.save("audio.mp3")
+        #z+=1
+        
+        return render_template('index.html', tw = tweet, time=j[l], sent = sent[0])
+    else:
+        return render_template('index.html')
+   
+@app.route("/scrape/audio/",methods=['GET'])
+def return_audio():
+    playsound("audio.mp3")
+    os.remove("audio.mp3")
+    return render_template('index.html')
+
+@app.route("/dashboard/",methods=['GET'])
+def return_plots():
+    today = dt.date.today()
+    yesterday = today - dt.timedelta(days = 1)
+    x=[0 for t in range(7)]
+    y=[0 for t in range(7)]
+    for m in range(7):
+        begin_date = yesterday - dt.timedelta(days = m)
+        end_date = today - dt.timedelta(days = m)
+        lang='en'
+        limit=500
+        tweets = query_tweets("COVID",begindate = begin_date, enddate = end_date ,limit = limit ,lang=lang)
+        df = pd.DataFrame(t.__dict__ for t in tweets)
+        i=df.text
+        j=df.timestamp
+        n = random.sample(range(0,len(i)),100)
+        
+        for k in n:
+            date = j[k]
+            tweet= i[k]
+            tweet_vector = vectorize.transform([tweet])
+            sent = classifier.predict(tweet_vector).tolist()
+            if(sent[0]==1):
+                x[m]=x[m]+1
+            else:
+                y[m]=y[m]+1
+    dates = [today-dt.timedelta(days = z) for z in range (7)]
+    x=np.array(x)
+    y=np.array(y)
+    plt.bar(dates, x, width=0.8, label='p', color='gold', bottom=y)
+    plt.bar(dates, y, width=0.8, label='n', color='silver')
+     
+    plt.ylim(0,120)
+    plt.ylabel("No. of Sentiments")
+    plt.xlabel("Date")
+    plt.xticks(rotation=25)
+    plt.legend(loc='centre', bbox_to_anchor=(1,0.60), ncol = 1)
+    plt.title("Sentiment Analysis")
+    
+    plt.savefig('C:/Users/KISHAN/Documents/Sentiment Analysis IBM/IBM/static/img/plot2.png')
+    plt.show()
+    colors = ['lightskyblue', 'lightcoral']
+    labels = ['Positive', 'Negative',]
+    sizes=[sum(x),sum(y)]
+    explode = (0.2, 0)
+    patches, texts = plt.pie(sizes, explode=explode, colors=colors, shadow=True, startangle=45)
+    plt.legend(patches, labels, loc="best")
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.savefig('C:/Users/KISHAN/Documents/Sentiment Analysis IBM/IBM/static/img/plot3.png')
+    plt.show()
+    return render_template('index.html')
+    
+if __name__ == "__main__":
+    app.run(host="localhost", port=8000, debug=True)
---
+++ model.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 17:21:38 2020
+
+@author: KISHAN
+"""
+
+import pandas as pd
+import numpy as np
+import re
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import LancasterStemmer
+from nltk.stem import WordNetLemmatizer
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection,svm
+from sklearn.metrics import accuracy_score
+
+dataset = pd.read_csv("tweets2.csv",encoding='latin-1',low_memory=False)
+#dataset.head()
+
+dataset['sentiment_text'] = dataset['sentiment_text'].str.lower() 
+
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt)
+        
+    return input_txt    
+dataset['sentiment_text'] = np.vectorize(remove_pattern)(dataset['sentiment_text'], "@[\w]*")
+dataset['sentiment_text'] = dataset['sentiment_text'].str.replace("[^a-zA-Z#]", " ")
+dataset['sentiment_text'] = dataset['sentiment_text'].apply(lambda x: ' '.join([w for w in x.split() if len(w)>3]))
+print("Done")
+
+dataset['sentiment_text']=[word_tokenize(entry) for entry in dataset['sentiment_text']]
+print("Done")
+tag_map = defaultdict(lambda : wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+i=0
+for index,entry in enumerate(dataset['sentiment_text']):
+    # Declaring Empty List to store the words that follow the rules for this step
+    Final_words = []
+    # Initializing WordNetLemmatizer()
+    word_Lemmatized = WordNetLemmatizer()
+    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
+    for word, tag in pos_tag(entry):
+        # Below condition is to check for Stop words and consider only alphabets
+        if word not in stopwords.words('english') and word.isalpha():
+            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
+            Final_words.append(word_Final)
+    # The final processed set of words for each iteration will be stored in 'text_final'
+    dataset.loc[index,'text_final'] = str(Final_words)
+    i=i+1
+    print(i)
+#dataset['sentiment_text']=LancasterStemmer.stem(dataset['sentiment_text'])
+print(dataset['text_final'])
+
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(dataset['text_final'],dataset['sentiment'],test_size=0.3)
+
+Tfidf_vect = TfidfVectorizer(max_features=100000)
+Tfidf_vect.fit(dataset['text_final'])
+Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+
+pickle.dump(Tfidf_vect, open('VECT.sav','wb'))
+print("Done")
+
+print(Tfidf_vect.vocabulary_)
+print(Train_X_Tfidf)
+
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma = 'auto')
+SVM.fit(Train_X_Tfidf,Train_Y)
+
+pickle.dump(SVM, open('SVM.sav','wb'))
+print("Done")    
+    
+# predict the labels on validation dataset
+predictions_SVM = SVM.predict(Test_X_Tfidf)
+# Use accuracy_score function to get the accuracy
+print("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, Test_Y)*100)
